128426ac4111c0f6cd2c650d0c562765b5c7aac3
Jakub Niedźwiedź <jakub.niedzwiedz98@gmail.com>
2024-03-24 05:52:33 +0100

    Add plug.gd file
    Here's how to use it: https://github.com/imjp94/gd-plug/blob/master/README.md

---
+++ src/plug.gd
@@ -0,0 +1,19 @@
+extends "res://addons/gd-plug/plug.gd"
+
+func _plugging():
+	
+	# qol
+	plug("NoctemCat/BottomPanelShortcuts")
+	
+	# planning
+	plug("HolonProduction/godot_kanban_tasks", {"exclude": ["addons/standalone_tools"]})
+	plug("OrigamiDev-Pete/TODO_Manager")
+	
+	# debug
+	# plug("russmatney/log.gd")
+	plug("Ericdowney/SignalVisualizer")
+	
+	# shaders
+	plug("arkology/ShaderV")
+	plug("paddy-exe/GodotVisualShader-Extras")
+	plug("kubaxius/SDFAddon")
